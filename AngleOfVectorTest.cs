3409a3f637722d2cae3b5b944cae8b3a965584e9
Austin Matthews <amatt1552@gmail.com>
2018-12-11 04:50:53 -0500

    first Commit!

---
+++ AngleOfVectorTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleOfVectorTest : MonoBehaviour
+{
+    
+    public Transform posA;
+    public Transform posB;
+    void Update ()
+    {
+
+        
+        Vector3 diff = posB.position - posA.position;
+        
+        print(Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg);
+	}
+}
